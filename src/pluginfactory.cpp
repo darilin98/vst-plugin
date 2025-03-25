cb6413140c94771766c48c43006fc84681340c4e
darilin98 <darekrudis@email.cz>
2025-03-25 20:18:39 +0100

    Add plugin metadata definition

--- src/pluginfactory.cpp
+++ src/pluginfactory.cpp
@@ -1,3 +1,53 @@
 //
 // Created by Darek Rudiš on 20.03.2025.
 //
+#include "public.sdk/source/main/pluginfactory.h"
+#include "public.sdk/source/vst/utility/stringconvert.h"
+#include "public.sdk/source/vst/vstcomponent.h"
+#include "public.sdk/source/vst/vsteditcontroller.h"
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/base/ustring.h"
+#include "pluginterfaces/vst/ivstaudioprocessor.h"
+#include "pluginterfaces/vst/ivsteditcontroller.h"
+
+#include "processor.hpp"
+#include "controller.hpp"
+
+using namespace Steinberg::Vst;
+
+// TEMPORARY - should be generated random IDs
+FUID PluginProcessorUID = FUID::fromTUID("9CC5B171-97F5-4ADB-9F65-013A306C73C2");
+FUID PluginControllerUID = FUID::fromTUID("DA511324-A388-46E0-B50E-DCCDE24FD078");
+
+BEGIN_FACTORY_DEF("MyCompany",
+                  "https://mycompany.com",
+                  "mailto:contact@mycompany.com")
+
+    // Register the processor
+    DEF_CLASS2(INLINE_UID_FROM_FUID(PluginProcessorUID),
+               PClassInfo::kManyInstances, // multiple instances allowed
+               kVstAudioEffectClass,       // Component category
+               "My Plugin Processor",      // Plugin name
+               Vst::kDistributable,        // Distributable attribute
+               "Fx|Synth",                 // Subcategories
+               "1.0.0",                     // Version
+               "MyCompany",                // Vendor
+               PluginProcessor::createInstance)
+
+    // Register the controller
+    DEF_CLASS2(INLINE_UID_FROM_FUID(PluginControllerUID),
+               PClassInfo::kManyInstances,
+               kVstComponentControllerClass,
+               "My Plugin Controller",
+               0,
+               "",
+               "1.0.0",
+               "MyCompany",
+               PluginController::createInstance)
+
+END_FACTORY
+
+FUID bundleEntry() {
+    return PluginProcessorUID;  // Return the FUID of the main processor class
+}
+

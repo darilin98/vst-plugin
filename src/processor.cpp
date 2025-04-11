80cdc549dc63e90c16bf58063cd1e97f12ab690b
darilin98 <darekrudis@email.cz>
2025-04-11 17:08:33 +0200

    First fft equalizer experiment

--- src/processor.cpp
+++ src/processor.cpp
@@ -3,6 +3,8 @@
 //
 #include "processor.hpp"
 
+#include <sys/proc.h>
+
 tresult PLUGIN_API PluginProcessor::initialize(FUnknown *context)
 {
     tresult result = AudioEffect::initialize(context);
@@ -23,23 +25,50 @@ tresult PLUGIN_API PluginProcessor::process(ProcessData& data)
     if (numSamples == 0)
         return kResultOk;
 
+    float sampleRate = this->processSetup.sampleRate;
+
     // Assuming only mono input for now
     float* input = data.inputs[0].channelBuffers32[0];
+    float* output = data.outputs[0].channelBuffers32[0];
 
-    auto* out = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * (numSamples /2 + 1));
     auto* in = (float*)fftwf_malloc(sizeof(float) * numSamples);
+    auto* out = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * (numSamples /2 + 1));
+    auto* processed = (float*)fftwf_malloc(sizeof(float) * numSamples);
 
     std::copy(input, input + numSamples, in);
-    fftwf_plan plan = fftwf_plan_dft_r2c_1d(numSamples, in, out, FFTW_ESTIMATE);
+    fftwf_plan planFwd = fftwf_plan_dft_r2c_1d(numSamples, in, out, FFTW_ESTIMATE);
+    fftwf_plan planInv = fftwf_plan_dft_c2r_1d(numSamples, out, processed, FFTW_ESTIMATE);
 
-    fftwf_execute(plan);
+    fftwf_execute(planFwd);
 
-    fftwf_destroy_plan(plan);
-    fftwf_free(out);
+    for (int i = 0; i < numSamples / 2 + 1; ++i) {
+        float freq = (i * sampleRate) / numSamples;
+
+        float gain = 1.0f;
+        if (freq > 100 && freq < 200)
+            gain = 0.5f;  // cut
+        else if (freq > 1000 && freq < 3000)
+            gain = 2.0f;  // boost
+
+        out[i][0] *= gain;  // real part
+        out[i][1] *= gain;  // imag part
+    }
+
+    fftwf_execute(planInv);
+
+    for (int i = 0; i < numSamples; ++i)
+        output[i] = processed[i] / numSamples;
+
+    fftwf_destroy_plan(planFwd);
+    fftwf_destroy_plan(planInv);
+
     fftwf_free(in);
+    fftwf_free(out);
+    fftwf_free(processed);
 
     return kResultOk;
 }
+
 tresult PLUGIN_API PluginProcessor::getControllerClassId(TUID classId)
 {
     if (!classId)

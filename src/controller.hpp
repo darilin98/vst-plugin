d8c21fcf1542fbdfb831866b1e74f08aa8fa1a10
darilin98 <darekrudis@email.cz>
2025-04-24 19:42:59 +0200

    Define required methods for main Bypass param

--- src/controller.hpp
+++ src/controller.hpp
@@ -25,5 +25,8 @@ public:
 
     tresult PLUGIN_API initialize(FUnknown* context) SMTG_OVERRIDE;
     tresult PLUGIN_API terminate() SMTG_OVERRIDE;
+    tresult PLUGIN_API getState(IBStream *state) SMTG_OVERRIDE;
+    tresult PLUGIN_API setState(IBStream*) SMTG_OVERRIDE;
+    ParamValue bypassState = 0.0;
 };
 #endif //CONTROLLER_HPP

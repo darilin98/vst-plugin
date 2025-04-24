a4c9db3c658c66d15577cb123367c28a3725eb32
darilin98 <darekrudis@email.cz>
2025-04-24 19:44:06 +0200

    Add temporary helper debug logger method

--- src/fftprocessor.hpp
+++ src/fftprocessor.hpp
@@ -5,6 +5,7 @@
 #include <fftw3.h>
 #include <algorithm>
 #include <vector>
+#include <chrono>
 #include "base/ftypes.h"
 
 #ifndef FFTPROCESSOR_HPP
@@ -31,4 +32,14 @@ private:
     fftwf_plan plan_fwd_;
     fftwf_plan plan_inv_;
 };
+
+#include <fstream>
+
+inline void log_debug(const std::string& msg)
+{
+    auto now = std::chrono::system_clock::now();
+    std::string formatted_time = std::format("{0:%F_%T}", now);
+    std::ofstream log("/Users/darek/Documents/vst3_debug_log.txt", std::ios::app);
+    log << formatted_time << ": "<< msg << std::endl;
+}
 #endif //FFTPROCESSOR_HPP

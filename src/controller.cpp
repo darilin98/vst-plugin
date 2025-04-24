509a40bad2c4cd183003ede8ff2f515f55d6fb29
darilin98 <darekrudis@email.cz>
2025-04-24 19:51:46 +0200

    Define bypass param in controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -9,7 +9,10 @@ tresult PLUGIN_API PluginController::initialize(FUnknown* context)
     if (result != kResultOk)
         return result;
     parameters.removeAll();
-    parameters.addParameter(STR16("Gain"), nullptr, 0, 0.5, ParameterInfo::kCanAutomate, 100);
+    parameters.addParameter(STR16("Gain"), nullptr, 0, 0.5, ParameterInfo::kCanAutomate, kParamGain);
+    // https://forums.steinberg.net/t/host-bypass-vst2-vs-vst3-explanation/201874?utm=true
+    parameters.addParameter(STR16("Bypass"), nullptr, 1, 0.0, ParameterInfo::kIsBypass | ParameterInfo::kCanAutomate | ParameterInfo::kIsList, kParamBypass);
+
     return kResultOk;
 }
 

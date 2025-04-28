cb897705fc57b18dfe42e74f7d57108dbb2bc7cf
darilin98 <darekrudis@email.cz>
2025-04-28 20:59:08 +0200

    Add dummy modulate method

--- src/equalizer.cpp
+++ src/equalizer.cpp
@@ -1,3 +1,16 @@
 //
 // Created by Darek Rudiš on 25.04.2025.
 //
+#include "equalizer.hpp"
+
+void PolynomialEqualizer::modulate(fftw_complex *freq_bins, int fft_size, int sample_rate)
+{
+    for (int i = 0; i < fft_size / 2 + 1; ++i) {
+        float freq = (i * sample_rate) / fft_size;
+
+        int gain = 1.0f;
+
+        freq_bins[i][0] *= gain;  // real part
+        freq_bins[i][1] *= gain;  // imag part
+    }
+}

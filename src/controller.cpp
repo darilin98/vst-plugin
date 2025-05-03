9c8eb19afffda083390ef5e68a69ffe10a2a5018
darilin98 <darekrudis@email.cz>
2025-05-03 16:46:56 +0200

    Declare new parameters

--- src/controller.cpp
+++ src/controller.cpp
@@ -11,9 +11,13 @@ tresult PLUGIN_API PluginController::initialize(FUnknown* context)
     if (result != kResultOk)
         return result;
     parameters.removeAll();
-    parameters.addParameter(STR16("Gain"), nullptr, 0, 0.5, ParameterInfo::kCanAutomate, kParamGain);
+    // parameters.addParameter(STR16("Gain"), nullptr, 0, 0.5, ParameterInfo::kCanAutomate, kParamGain);
     // https://forums.steinberg.net/t/host-bypass-vst2-vs-vst3-explanation/201874?utm=true
     parameters.addParameter(STR16("Bypass"), nullptr, 1, 0.0, ParameterInfo::kIsBypass | ParameterInfo::kCanAutomate | ParameterInfo::kIsList, kParamBypass);
+    parameters.addParameter(STR16("Shift"), nullptr, 0, 0.5, ParameterInfo::kCanAutomate, kParamShift);
+    parameters.addParameter(STR16("Alpha"), nullptr, 0, 0.5, ParameterInfo::kCanAutomate, kParamAlpha);
+    parameters.addParameter(STR16("Beta"), nullptr, 0, 0.5, ParameterInfo::kCanAutomate, kParamBeta);
+
 
     return kResultOk;
 }

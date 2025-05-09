81fc93aca3bc436ae266daf6c3a137e2dfd604f8
darilin98 <darekrudis@email.cz>
2025-05-09 14:52:15 +0200

    Quadratic function experiment

--- src/equalizer.cpp
+++ src/equalizer.cpp
@@ -5,15 +5,15 @@
 
 void PolynomialEqualizer::modulate(fftwf_complex *freq_bins, int fft_size, int sample_rate)
 {
+    float centerFreq = 20000 * param_shift_;
+    float dBpeakGain = 12;
     for (int i = 0; i < fft_size / 2 + 1; ++i) {
         float freq = (i * sample_rate) / fft_size;
 
-        float gain = 1.0f;
-        float cut_center = 600.0f, cut_width = 300.0f;
-        gain *= 1.0f - 0.9f * expf(-powf((freq - cut_center) / cut_width, 2.0f));
+        float x = (freq - centerFreq) / centerFreq;
+        float dBgain = param_alpha_ * x * x + dBpeakGain;
 
-        float boost_center = 4500.0f, boost_width = 800.0f;
-        gain *= 1.0f + 5.0f * expf(-powf((freq - boost_center) / boost_width, 2.0f));
+        float gain = powf(10.0f, dBgain / 20.0f);
 
         freq_bins[i][0] *= gain;  // real part
         freq_bins[i][1] *= gain;  // imag part

62bad812a5892c05ded6653a2631b21b45ff82a0
darilin98 <darekrudis@email.cz>
2025-05-14 19:52:06 +0200

    Add more interesting polynomial to modulate method

--- src/equalizer.cpp
+++ src/equalizer.cpp
@@ -6,19 +6,21 @@
 void PolynomialEqualizer::modulate(fftwf_complex *freq_bins, int fft_size, int sample_rate)
 {
     const float minFreq = 20.f;
-    const float maxFreq = sample_rate * 0.5f;
+    const float maxFreq = sample_rate * 0.5f; // Nyquist frequency
+    constexpr float dBMaxBoost = 12.0f;
 
-    float centerFreq = expf( logf(minFreq) + param_shift_ * (logf(maxFreq) - logf(minFreq)) );
+    float centerFreq = expf( logf(minFreq) + param_shift_ * (logf(maxFreq) - logf(minFreq)) ); // Knob has a logarithmic feel -> converting back to linear
     float flip = 1.0f - 2.0f * param_direction_;
-    float dBpeakGain = flip * param_intensity_ * 12.0f; //12 dB max boost
+    float dBpeakGain = flip * param_intensity_ * dBMaxBoost;
 
     for (int i = 0; i < fft_size / 2 + 1; ++i) {
         float freq = (i * sample_rate) / fft_size;
 
-        // Normalize curve so that values way beyond the target range are left intact
+        // Normalize curve so that high/low frequencies get treated equally
         float x = (freq - centerFreq) / (centerFreq * param_width_);
 
-        float shape = 1.0f - x*x;
+        //float shape = 1.0f - x*x;
+        float shape = -powf(x, 4.0f) + powf(x, 3.0f) + powf(x, 2.0f) + 0.5f;
         if (shape < 0.0f) shape = 0.0f;
 
         float dBgain = dBpeakGain * shape;

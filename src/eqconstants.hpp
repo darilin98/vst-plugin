2c9d7ae8eb4a61b39f65c5ec3239941d7a148431
darilin98 <darekrudis@email.cz>
2025-07-04 16:44:09 +0200

    Group EQ constants into one file

---
+++ src/eqconstants.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Darek Rudiš on 04.07.2025.
+//
+
+#ifndef EQCONSTANTS_HPP
+#define EQCONSTANTS_HPP
+
+namespace EQ {
+    constexpr float minFreq = 20.0f;
+    constexpr float maxFreq = 20000.0f;
+
+    constexpr float dBMaxBoost = 12.0f;
+    constexpr float kDefaultShift = 0.5f;
+    constexpr float kDefaultIntensity = 0.5f;
+    constexpr float kDefaultDirection = 0.0f;
+    constexpr float kDefaultWidth = 0.5f;
+
+}
+
+#endif //EQCONSTANTS_HPP
